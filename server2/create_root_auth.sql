8a7942b3b8cb042cff7a3643b07d747a67b523b4
Seochangbin <tjaudqls@gmail.com>
2021-07-28 08:30:23 +0000

    Update create_root_auth.sql

--- server2/create_root_auth.sql
+++ server2/create_root_auth.sql
@@ -1,4 +1,4 @@
 create user 'atcs02'@'%' identified by 'xhdtlsqhdks1';
 grant all privileges on tcs_database.* to 'atcs02'@'%';
-grant all privileges on *.* to 'root'@'%' identified by 'xhdtlsqhdks1';
+/*grant all privileges on *.* to 'root'@'%' identified by 'xhdtlsqhdks1';*/
 FLUSH privileges;
