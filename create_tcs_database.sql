afa52f2664e4cde619fe74199883388cf2792961
Seochangbin <tjaudqls@gmail.com>
2021-10-22 06:53:33 +0000

    Update create_tcs_database.sql (2021/10/22)

--- create_tcs_database.sql
+++ create_tcs_database.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- 호스트:                          58.72.35.74
--- 서버 버전:                        10.5.9-MariaDB - MariaDB Server
+-- 호스트:                          192.168.1.182
+-- 서버 버전:                        10.5.12-MariaDB - MariaDB Server
 -- 서버 OS:                        Linux
--- HeidiSQL 버전:                  11.0.0.5919
+-- HeidiSQL 버전:                  11.3.0.6295
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,6 +10,7 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- tcs_database 데이터베이스 구조 내보내기
@@ -24,12 +25,17 @@ CREATE TABLE IF NOT EXISTS `camera` (
   `camera_x` int(11) DEFAULT NULL COMMENT 'X축 px값',
   `camera_y` int(11) DEFAULT NULL COMMENT 'Y축 px값',
   `angle` int(11) DEFAULT NULL COMMENT 'Angle px값',
-  `url` varchar(500) DEFAULT NULL COMMENT '카메라 URL(http)',
+  `url` varchar(500) DEFAULT NULL COMMENT '영상처리 스트림',
+  `camUrl` varchar(500) DEFAULT NULL COMMENT '카메라스트림',
   KEY `FK_camera_local` (`id`),
   CONSTRAINT `FK_camera_local` FOREIGN KEY (`id`) REFERENCES `local` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.camera:~1 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `camera` DISABLE KEYS */;
+INSERT INTO `camera` (`idx`, `id`, `num`, `camera_x`, `camera_y`, `angle`, `url`, `camUrl`) VALUES
+	(1, 1, 1, 102, -20, -93, 'http://14.51.232.239:3300/stream/mjpeg?id=1&num=1', 'http://14.51.232.239:3300/stream/camera?id=1&num=1');
+/*!40000 ALTER TABLE `camera` ENABLE KEYS */;
 
 -- 테이블 tcs_database.center_config 구조 내보내기
 CREATE TABLE IF NOT EXISTS `center_config` (
@@ -40,9 +46,13 @@ CREATE TABLE IF NOT EXISTS `center_config` (
   PRIMARY KEY (`server_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
-
-
+-- 테이블 데이터 tcs_database.center_config:~3 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `center_config` DISABLE KEYS */;
+INSERT INTO `center_config` (`server_id`, `server_name`, `public_ip`, `private_ip`) VALUES
+	(1, '장흥 서버1', '14.51.232.239', '192.168.1.181'),
+	(2, '장흥 서버2', '14.51.232.239', '192.168.1.182'),
+	(3, '장흥 서버3', '14.51.232.239', '192.168.1.183');
+/*!40000 ALTER TABLE `center_config` ENABLE KEYS */;
 
 -- 테이블 tcs_database.crossNodeApi 구조 내보내기
 CREATE TABLE IF NOT EXISTS `crossNodeApi` (
@@ -50,7 +60,9 @@ CREATE TABLE IF NOT EXISTS `crossNodeApi` (
   `COMMENT` varchar(20) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '추가설명'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.crossNodeApi:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `crossNodeApi` DISABLE KEYS */;
+/*!40000 ALTER TABLE `crossNodeApi` ENABLE KEYS */;
 
 -- 테이블 tcs_database.cycle_log 구조 내보내기
 CREATE TABLE IF NOT EXISTS `cycle_log` (
@@ -80,7 +92,9 @@ CREATE TABLE IF NOT EXISTS `cycle_log` (
   CONSTRAINT `cycle_log_ibfk_1` FOREIGN KEY (`id`) REFERENCES `local` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1548915 DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.cycle_log:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `cycle_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cycle_log` ENABLE KEYS */;
 
 -- 테이블 tcs_database.detect_log 구조 내보내기
 CREATE TABLE IF NOT EXISTS `detect_log` (
@@ -110,7 +124,9 @@ CREATE TABLE IF NOT EXISTS `detect_log` (
   CONSTRAINT `detect_log_ibfk_1` FOREIGN KEY (`id`) REFERENCES `local` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1548915 DEFAULT CHARSET=utf8;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.detect_log:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `detect_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detect_log` ENABLE KEYS */;
 
 -- 테이블 tcs_database.detect_sensor 구조 내보내기
 CREATE TABLE IF NOT EXISTS `detect_sensor` (
@@ -118,7 +134,7 @@ CREATE TABLE IF NOT EXISTS `detect_sensor` (
   `name` varchar(20) DEFAULT NULL,
   `id` int(11) NOT NULL COMMENT '교차로번호',
   `video_link` varchar(200) DEFAULT NULL COMMENT '스트리밍URL',
-  `map_link` varchar(200) DEFAULT NULL,
+  `ogCamera_link` varchar(200) DEFAULT NULL,
   `device_ip` varchar(20) DEFAULT NULL COMMENT '장치 IP',
   `Detect_Number` int(11) DEFAULT NULL COMMENT '검지기 번호',
   `device_type` int(11) NOT NULL DEFAULT 1 COMMENT '장치 타입',
@@ -129,19 +145,11 @@ CREATE TABLE IF NOT EXISTS `detect_sensor` (
   CONSTRAINT `detect_sensor_ibfk_2` FOREIGN KEY (`device_type`) REFERENCES `device` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
-
--- 테이블 tcs_database.det_error 구조 내보내기
-CREATE TABLE IF NOT EXISTS `det_error` (
-  `seq` int(11) NOT NULL AUTO_INCREMENT,
-  `log_time` varchar(50) DEFAULT NULL,
-  `log_name` varchar(50) DEFAULT NULL,
-  `lcid` varchar(50) DEFAULT NULL,
-  `det_id` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`seq`)
-) ENGINE=MyISAM AUTO_INCREMENT=197 DEFAULT CHARSET=utf8;
-
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.detect_sensor:~1 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `detect_sensor` DISABLE KEYS */;
+INSERT INTO `detect_sensor` (`d_no`, `name`, `id`, `video_link`, `ogCamera_link`, `device_ip`, `Detect_Number`, `device_type`) VALUES
+	(1, '용진삼거리 1번', 1, 'http://14.51.232.120:7777/', 'http://admin:dongbuict0@223.171.56.51:1080/streaming/channels/http/0', '14.51.232.120', 1, 4);
+/*!40000 ALTER TABLE `detect_sensor` ENABLE KEYS */;
 
 -- 테이블 tcs_database.device 구조 내보내기
 CREATE TABLE IF NOT EXISTS `device` (
@@ -150,7 +158,22 @@ CREATE TABLE IF NOT EXISTS `device` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.device:~12 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `device` DISABLE KEYS */;
+INSERT INTO `device` (`id`, `NAME`) VALUES
+	(1, '서버'),
+	(2, '제어기'),
+	(3, '검지기'),
+	(4, '1번 NANO'),
+	(5, '1번 CAMERA'),
+	(6, '2번 NANO'),
+	(7, '2번 CAMERA'),
+	(8, '3번 NANO'),
+	(9, '3번 CAMERA'),
+	(10, '4번 NANO'),
+	(11, '4번 CAMERA'),
+	(12, '라우터');
+/*!40000 ALTER TABLE `device` ENABLE KEYS */;
 
 -- 테이블 tcs_database.event_code 구조 내보내기
 CREATE TABLE IF NOT EXISTS `event_code` (
@@ -159,7 +182,39 @@ CREATE TABLE IF NOT EXISTS `event_code` (
   PRIMARY KEY (`event_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.event_code:~29 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `event_code` DISABLE KEYS */;
+INSERT INTO `event_code` (`event_code`, `event_name`) VALUES
+	(0, '제어기 통신상태'),
+	(1, 'CAN 상태'),
+	(2, 'LTE 통신상태'),
+	(3, '예비_03'),
+	(4, '예비_04'),
+	(5, '조광제어'),
+	(6, 'SCU 통신'),
+	(7, 'Power Fail'),
+	(8, 'DATABASE'),
+	(9, '점멸'),
+	(10, '소등'),
+	(11, '모순'),
+	(12, '소등 스위치'),
+	(13, '점멸 스위치'),
+	(14, '수동 스위치'),
+	(15, '수동진행 스위치'),
+	(16, 'DOOR'),
+	(17, '모순검지'),
+	(18, '수동제어'),
+	(19, '시차제'),
+	(20, '예비_20'),
+	(21, '예비_21'),
+	(22, '예비_22'),
+	(23, '푸쉬버튼'),
+	(24, '예비_24'),
+	(25, '검지기 1번'),
+	(26, '예비_26'),
+	(27, '검지기 2번'),
+	(99, 'RESTART');
+/*!40000 ALTER TABLE `event_code` ENABLE KEYS */;
 
 -- 테이블 tcs_database.event_log 구조 내보내기
 CREATE TABLE IF NOT EXISTS `event_log` (
@@ -176,7 +231,9 @@ CREATE TABLE IF NOT EXISTS `event_log` (
   CONSTRAINT `event_log_ibfk_2` FOREIGN KEY (`device`) REFERENCES `device` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21276 DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.event_log:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `event_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_log` ENABLE KEYS */;
 
 -- 테이블 tcs_database.event_status 구조 내보내기
 CREATE TABLE IF NOT EXISTS `event_status` (
@@ -187,16 +244,81 @@ CREATE TABLE IF NOT EXISTS `event_status` (
   CONSTRAINT `event_status_ibfk_1` FOREIGN KEY (`event_code`) REFERENCES `event_code` (`event_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.event_status:~58 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `event_status` DISABLE KEYS */;
+INSERT INTO `event_status` (`event_code`, `value`, `status`) VALUES
+	(0, 0, '통신on'),
+	(0, 1, '통신off'),
+	(1, 0, 'CAN_정상'),
+	(1, 1, 'CAN_고장'),
+	(2, 0, 'ONLINE'),
+	(2, 1, 'OFFLINE'),
+	(3, 0, '0'),
+	(3, 1, '1'),
+	(4, 1, '1'),
+	(4, 0, '0'),
+	(5, 1, 'ON'),
+	(5, 0, 'OFF'),
+	(6, 1, '이상'),
+	(6, 0, '정상'),
+	(7, 0, '정상'),
+	(7, 1, '이상'),
+	(8, 0, '정상'),
+	(8, 1, '이상'),
+	(9, 0, '점멸 종료'),
+	(9, 1, '점멸 시작'),
+	(10, 0, '소등 종료'),
+	(10, 1, '소등 시작'),
+	(11, 0, '해제'),
+	(11, 1, '모순'),
+	(12, 0, '수동 수등 종료'),
+	(12, 1, '수동 소등 시작'),
+	(13, 0, '수동 점멸 종료'),
+	(13, 1, '수동 점멸 시작'),
+	(14, 0, '수동 종료'),
+	(14, 1, '수동 시작'),
+	(15, 0, 'x'),
+	(15, 1, '수동 버튼 누름'),
+	(16, 0, '닫힘'),
+	(16, 1, '열림'),
+	(17, 0, '금지'),
+	(17, 1, '허용'),
+	(18, 0, '수동금지'),
+	(18, 1, '수동허용'),
+	(19, 0, '종료'),
+	(19, 1, '시작'),
+	(20, 0, '0'),
+	(20, 1, '1'),
+	(21, 0, '0'),
+	(21, 1, '1'),
+	(22, 0, '0'),
+	(22, 1, '1'),
+	(23, 0, 'ENABLE'),
+	(23, 1, 'DISABLE'),
+	(24, 0, '0'),
+	(24, 1, '1'),
+	(25, 0, '0'),
+	(25, 1, '1'),
+	(26, 0, '0'),
+	(26, 1, '1'),
+	(27, 0, '0'),
+	(27, 1, '1'),
+	(99, 0, '0'),
+	(99, 1, '1');
+/*!40000 ALTER TABLE `event_status` ENABLE KEYS */;
 
 -- 테이블 tcs_database.group_table 구조 내보내기
 CREATE TABLE IF NOT EXISTS `group_table` (
   `group_no` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `group_name` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
   PRIMARY KEY (`group_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COMMENT='교차로 그룹 테이블';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COMMENT='교차로 그룹 테이블';
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.group_table:~1 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `group_table` DISABLE KEYS */;
+INSERT INTO `group_table` (`group_no`, `group_name`) VALUES
+	(1, '1번 그룹');
+/*!40000 ALTER TABLE `group_table` ENABLE KEYS */;
 
 -- 테이블 tcs_database.local 구조 내보내기
 CREATE TABLE IF NOT EXISTS `local` (
@@ -207,8 +329,6 @@ CREATE TABLE IF NOT EXISTS `local` (
   `latitude` varchar(50) NOT NULL,
   `longitude` varchar(50) NOT NULL,
   `group_id` int(11) unsigned DEFAULT NULL,
-  `traffic_latitude` varchar(50) DEFAULT NULL,
-  `traffic_longitude` varchar(50) DEFAULT NULL,
   `traffic_x` int(11) DEFAULT NULL,
   `traffic_y` int(11) DEFAULT NULL,
   `maps` varchar(100) DEFAULT NULL,
@@ -223,17 +343,12 @@ CREATE TABLE IF NOT EXISTS `local` (
   CONSTRAINT `FK_local_group_table` FOREIGN KEY (`group_id`) REFERENCES `group_table` (`group_no`)
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
-
--- 테이블 tcs_database.local_status 구조 내보내기
-CREATE TABLE IF NOT EXISTS `local_status` (
-  `id` int(11) NOT NULL,
-  `status` int(11) unsigned NOT NULL DEFAULT 1,
-  KEY `id` (`id`),
-  CONSTRAINT `local_status_ibfk_1` FOREIGN KEY (`id`) REFERENCES `local` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.local:~2 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `local` DISABLE KEYS */;
+INSERT INTO `local` (`id`, `ip`, `name`, `Ver_2004`, `latitude`, `longitude`, `group_id`, `traffic_x`, `traffic_y`, `maps`, `markers`, `offline`, `online`, `cross_type`, `detector_count`, `prevNode_distance`) VALUES
+	(1, '192.168.1.203', '회사1', 0, '126.96944239470625', '37.390796490308624', 1, -29, -28, 'map_1.png', 'marker-default_1.png', 'marker-offline_1.png', 'marker-online_1.png', 3, 1, 0),
+	(2, '192.168.1.42', '회사2', 0, '126.96886149246555', '37.396017763471434', 1, -50, -50, 'map_2.png', 'marker-default_2.png', 'marker-offline_2.png', 'marker-online_2.png', 3, 1, 0);
+/*!40000 ALTER TABLE `local` ENABLE KEYS */;
 
 -- 테이블 tcs_database.network_report 구조 내보내기
 CREATE TABLE IF NOT EXISTS `network_report` (
@@ -253,7 +368,9 @@ CREATE TABLE IF NOT EXISTS `network_report` (
   CONSTRAINT `network_report_ibfk_1` FOREIGN KEY (`id`) REFERENCES `local` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12528111 DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.network_report:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `network_report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `network_report` ENABLE KEYS */;
 
 -- 테이블 tcs_database.network_report_console 구조 내보내기
 CREATE TABLE IF NOT EXISTS `network_report_console` (
@@ -271,7 +388,9 @@ CREATE TABLE IF NOT EXISTS `network_report_console` (
   CONSTRAINT `network_report_console_ibfk_1` FOREIGN KEY (`id`) REFERENCES `local` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.network_report_console:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `network_report_console` DISABLE KEYS */;
+/*!40000 ALTER TABLE `network_report_console` ENABLE KEYS */;
 
 -- 테이블 tcs_database.traffic 구조 내보내기
 CREATE TABLE IF NOT EXISTS `traffic` (
@@ -288,7 +407,14 @@ CREATE TABLE IF NOT EXISTS `traffic` (
   CONSTRAINT `FK_traffic_local` FOREIGN KEY (`id`) REFERENCES `local` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.traffic:~4 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `traffic` DISABLE KEYS */;
+INSERT INTO `traffic` (`id`, `phase`, `arrow`, `angle`, `top`, `left`, `unityX`, `unityY`, `unityZ`) VALUES
+	(1, 1, 'detour.png', -92, 35, 36, 180, 0, 0),
+	(1, 2, 'left.png', -93, 35, 36, 180, 0, -90),
+	(1, 3, 'updown.png', -99, 35, 36, 180, 0, 0),
+	(1, 4, 'detour.png', -77, 35, 36, 180, 0, 0);
+/*!40000 ALTER TABLE `traffic` ENABLE KEYS */;
 
 -- 테이블 tcs_database.user 구조 내보내기
 CREATE TABLE IF NOT EXISTS `user` (
@@ -296,11 +422,23 @@ CREATE TABLE IF NOT EXISTS `user` (
   `id` varchar(50) NOT NULL,
   `pass` varchar(255) NOT NULL,
   `ip` varchar(50) DEFAULT NULL,
+  `role` varchar(10) DEFAULT NULL,
+  `date_created` varchar(50) DEFAULT NULL,
+  `created_by` varchar(50) DEFAULT NULL,
+  `date_modify` varchar(50) DEFAULT NULL,
+  `modifier` varchar(50) DEFAULT NULL,
+  `note` varchar(50) DEFAULT '',
   PRIMARY KEY (`no`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=102 DEFAULT CHARSET=utf8mb4;
 
--- 내보낼 데이터가 선택되어 있지 않습니다.
+-- 테이블 데이터 tcs_database.user:~2 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`no`, `id`, `pass`, `ip`, `role`, `date_created`, `created_by`, `date_modify`, `modifier`, `note`) VALUES
+	(1, 'superadmin', 'bdd277ea549604930717f20f4ca96d1753ad9d9b55d26c05519c061c3d584c7721d5e82312040eaff91b4924d85dc69e1f272878f1f261405eba3b93d7aa7076', '*', 'admin', NULL, 'superadmin', NULL, NULL, ''),
+	(57, 'police', 'bdd277ea549604930717f20f4ca96d1753ad9d9b55d26c05519c061c3d584c7721d5e82312040eaff91b4924d85dc69e1f272878f1f261405eba3b93d7aa7076', '*', 'user', '2021-10-05 14:10:40', 'superadmin', '2021-10-13 13:44:15', 'superadmin', '경찰 모니터링 계정');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
